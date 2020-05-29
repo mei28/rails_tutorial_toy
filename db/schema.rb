3dbe32ba65f43e702a3294f54686f78b0c4185f4
楊明哲 <mingzhe.yang@hcomp.cs.tsukuba.ac.jp>
2020-05-29 14:41:03 +0900

    scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_075546) do
+ActiveRecord::Schema.define(version: 2020_05_29_053912) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
